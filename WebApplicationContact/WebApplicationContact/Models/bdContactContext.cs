4eb09b65f6adcd055ca80f52a3b6d693ffa812d0
“cris2014971130” <andres.chamorro2012@live.com>
2022-02-17 19:45:27 -0500

    first commit

---
+++ WebApplicationContact/WebApplicationContact/Models/bdContactContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplicationContact.Models
+{
+    public partial class bdContactContext : DbContext
+    {
+        public bdContactContext()
+        {
+        }
+
+        public bdContactContext(DbContextOptions<bdContactContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=PC8F636\\SQLEXPRESS;Database=bdContact; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.Property(e => e.id).HasColumnName("id");
+
+                entity.Property(e => e.cellphone)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("cellphone");
+
+                entity.Property(e => e.name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.phone)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("phone");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
