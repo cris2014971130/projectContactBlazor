a63d6ece7aa191644121bf7b6642f26e17c6c32c
“cris2014971130” <andres.chamorro2012@live.com>
2022-02-25 09:47:47 -0500

    New Features

--- BlazorApp1/Server/Controllers/ContactController.cs
+++ BlazorApp1/Server/Controllers/ContactController.cs
@@ -1,41 +1,73 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using BlazorApp1.Shared;
+using System.Net;
 
-namespace BlazorApp1.Server.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ContactController : ControllerBase
-    {
-        private readonly ContactDbContext _context;
-        public ContactController(ContactDbContext context)
-        {
-            _context = context;
+namespace BlazorApp1.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ContactController : ControllerBase{
+    
+    private readonly ContactDbContext _context;
+    public ContactController(ContactDbContext context){
+        _context = context;
+    }
+
+    // GET: api/Contact
+    [HttpGet]
+    public IEnumerable<Contact> GetAllContacts(){
+        try{
+            return _context.Contacts.ToList();
+        }catch (Exception e){
+            throw;
         }
+    }
 
+    [HttpPost]
+    public IActionResult AddContact(Contact contact){
+        _context.Contacts.Add(contact);
+        _context.SaveChanges();
+        return Ok();
+    }
 
+    [HttpGet("{name}")]
+    public Contact GetContactByName(string name){
+        return _context.Contacts.SingleOrDefault(e => e.name == name);
+    }
 
-        // GET: api/Contact
-        [HttpGet]
-        public IEnumerable<Contact> GetAllEmployees()
-        {
-            try
-            {
-                return _context.Contacts.ToList();
+    [HttpGet("isExist/{name}")]
+    public bool isExist(string name){
+        print(name);
+        Contact c = _context.Contacts.SingleOrDefault(e => e.name == name);
+      
+        if(c != null){
+            print(name+ "" + c.ToString());
+            return true;
+        }
+        return false;
+    }
+
+    [HttpDelete("{name}")]
+    public string DeleteContact(string name){
+        Contact contact = _context.Contacts.Find(getContact(name));
+        if (contact == null){
+             return "NotFound";
+        }
+        _context.Contacts.Remove(contact);
+        _context.SaveChangesAsync();
+       return "jaja";
+    }
+
+    public int getContact(string name){
+        foreach (var item in _context.Contacts){
+            if (item.name.Equals(name)){
+                return item.id;
             }
-            catch (Exception ex)
-            {
-                throw;
-            }
         }
+        return -1;
+    }
 
-        [HttpPost]
-        public IActionResult AddContact(Contact contact)
-        {
-            _context.Contacts.Add(contact);
-            _context.SaveChanges();
-            return Ok(_context.Contacts.ToList());
-        }
+    public void print(string value){
+        Console.WriteLine(value);
     }
 }
