3a70b11f821142053aee194d5b886358f6dc570d
“cris2014971130” <andres.chamorro2012@live.com>
2022-02-28 16:56:24 -0500

    fix features, add validation and delete compilation files

--- BlazorApp1/Server/Controllers/ContactController.cs
+++ BlazorApp1/Server/Controllers/ContactController.cs
@@ -1,73 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BlazorApp1.Shared;
-using System.Net;
 
 namespace BlazorApp1.Server.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class ContactController : ControllerBase{
-    
+
     private readonly ContactDbContext _context;
+
     public ContactController(ContactDbContext context){
         _context = context;
     }
 
     // GET: api/Contact
-    [HttpGet]
-    public IEnumerable<Contact> GetAllContacts(){
-        try{
+    [HttpGet("list")]
+    public IEnumerable<Contact> GetAllContacts(){  
             return _context.Contacts.ToList();
-        }catch (Exception e){
-            throw;
-        }
     }
 
     [HttpPost]
-    public IActionResult AddContact(Contact contact){
+    public string AddContact(Contact contact){
         _context.Contacts.Add(contact);
         _context.SaveChanges();
-        return Ok();
+        return "Contacto Agregado.. Redireccionando a la Lista";
     }
 
     [HttpGet("{name}")]
     public Contact GetContactByName(string name){
-        return _context.Contacts.SingleOrDefault(e => e.name == name);
+        Contact contact = _context.Contacts.SingleOrDefault(e => e.name == name);
+        if(contact == null){
+            return new Contact();
+        }
+        return contact;
     }
 
     [HttpGet("isExist/{name}")]
     public bool isExist(string name){
-        print(name);
-        Contact c = _context.Contacts.SingleOrDefault(e => e.name == name);
-      
-        if(c != null){
-            print(name+ "" + c.ToString());
+        if(_context.Contacts.SingleOrDefault(e => e.name == name)!= null){
             return true;
         }
         return false;
     }
 
-    [HttpDelete("{name}")]
-    public string DeleteContact(string name){
-        Contact contact = _context.Contacts.Find(getContact(name));
-        if (contact == null){
-             return "NotFound";
-        }
-        _context.Contacts.Remove(contact);
-        _context.SaveChangesAsync();
-       return "jaja";
+    [HttpGet("space")]
+    public int spaceOcuped(){
+        return _context.Contacts.ToList().Count;
     }
 
-    public int getContact(string name){
+    [HttpPost("delete")]
+    public string DeleteContact(Contact _contact){
+        if (_contact == null){
+            return "Contacto No Encontrado";
+        }
+        _context.Contacts.Remove(_contact);
+        _context.SaveChangesAsync();
+        return "Contacto Encontrado y Eliminado Correctamente";
+    }
+
+    public int getContact(Contact _contact){
         foreach (var item in _context.Contacts){
-            if (item.name.Equals(name)){
+            if (item.name.Equals(_contact.name)){
                 return item.id;
             }
         }
         return -1;
-    }
-
-    public void print(string value){
-        Console.WriteLine(value);
     }
 }
--- BlazorApp1/Shared/Models/Contact.cs
+++ BlazorApp1/Shared/Models/Contact.cs
@@ -5,8 +5,16 @@ public class Contact{
 
     [Key]
     public int id           {get;set;}
+
+    [Required(ErrorMessage = "El nombre es Obligatorio")]
     public string name      {get;set;}
+
+    [Required(ErrorMessage = "El Telefono es Obligatorio")]
+    [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Por favor ingrese numeros validos")] 
     public string phone     {get;set;}
+    
+    [Required(ErrorMessage = "El Celular es Obligatorio")]
+    [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Por favor ingrese numeros validos")] 
     public string cellphone {get;set;}
 
     public override string ToString(){
